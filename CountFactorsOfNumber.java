5898ea7ca4463e6631bbbc33dbbc65038652c2f8
anandboge <anandreddyboge@gmail.com>
2024-03-03 19:42:11 +0530

    Count factors of a number

--- CountFactorsOfNumber.java
+++ CountFactorsOfNumber.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CountFactorsOfNum {
+public class CountFactorsOfNumber {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the number: ");
