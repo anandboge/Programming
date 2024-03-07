5295d98a2e80c53befbfa213c250e849acc83be6
anandboge <157228272+anandboge@users.noreply.github.com>
2024-03-07 18:50:45 +0530

    Update Diff21.java

--- Diff21.java
+++ Diff21.java
@@ -8,11 +8,11 @@ public class Diff21 {
     public static void main(String[] args) {
         System.out.println(diff21(19));
     }
-}
-public static int diff21(int n) {
+    public static int diff21(int n) {
     if (n <= 21)
      return 21 - n;
      
    else
      return (n-21) * 2;
  }
+}
